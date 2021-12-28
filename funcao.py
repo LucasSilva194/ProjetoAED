66d2cf5f4d8624e514deb99ea16f045d38340127
Lucas <lucasiilva03@gmail.com>
2021-12-28 16:10:01 +0000

    funcao com problemas

--- funcao.py
+++ funcao.py
@@ -1,47 +1,85 @@
-def Login():
-    user = txt_utilizador.get()
-    password = txt_passe.get()
-    
-    guardar = user + ";" + password
-    f = open("basedados.txt","r")
-    lista = f.readlines()
+from tkinter import *
+import tkinter.messagebox as tsmg
 
-    if str(guardar) in str(lista):
-        messagebox.showinfo("Bem vindo",f"Olá {user}, o seu login foi efetuado com sucesso")
-    else:
-        messagebox.showerror("Erro","Utilizador não encontrado. Por favor crie conta.")
+root=Tk()
 
-    txt_utilizador.set("")
-    txt_passe.set("")
-
-def CriarConta():
-    user = txt_utilizador,get()
-    password = txt_passe.get()
-    cpassword = txt_cpasse.get()
-
-    guardar = user + ";" + password
-    f = open("basedados.txt","r")
-    f.close()
-    if user != "" and password != "":
-        lista = f.readlines()
-        if str(guardar) in str(lista):
-            messagebox.showerror("Erro","Já existe uma conta com esses dados, por favor efetue login")
-            txt_utilizador.set("")
-            txt_passe.set("")
-        elif str(user) in str(lista):
-            messagebox.showerror("Erro","Esse utilizador já existe")
-            txt_utilizador.set("")
-            txt_passe.set("")
+def check():
+    a=uname.get()
+    b=pwd.get()
+    c=cpwd.get()
+    mix=a+"-"+b+"-"+c
+    with open("basedados.txt","r") as fi:
+        d=fi.readlines()
+    if b==c:
+        if str(mix) in str(d):
+            tsmg.showinfo("Welcome",f"Hello {a}, You Have SuccessFully Logged In")
         else:
-            if password == cpassword:
-                f = open("basedados.txt","a")
-                f.write(f"{user};{password}\n")
-                messagebox.showinfo("A sua conta foi criada com sucesso!")
-                txt_utilizador.set("")
-                txt_passe.set("")
-                txt_cpasse.set("")
+            tsmg.showerror("Error","No User Found, Please Sign-Up First")
+    else:
+        tsmg.showerror("Error","Both Password Are Different")
+    uname.set("")
+    pwd.set("")
+    cpwd.set("")
+
+def save():
+    a=uname.get()
+    b=pwd.get()
+    c=cpwd.get()
+    mix=a+"-"+b+"-"+c
+    if a!="" and b!="" and c!="" :
+        with open("Login.txt","r") as fi:
+            d=fi.readlines()
+        if str(mix) in str(d):
+            tsmg.showerror("Error","You Already Have An Account, Please Login")
+            uname.set("")
+            pwd.set("")
+            cpwd.set("")
+        elif str(a) in str(d):
+            tsmg.showerror("Error","Username Already Exist")
+            uname.set("")
+            pwd.set("")
+            cpwd.set("")
+        else:
+            if b==c:
+                with open("Login.txt","a") as f:
+                    f.write(f"{a}-{b}-{c}\n")
+                tsmg.showinfo("Success","Your Account Has Been Created")
+                uname.set("")
+                pwd.set("")
+                cpwd.set("")
             else:
-                messagebox.showerror("Erro","As duas passwords não coincidem")
-                txt_utilizador.set("")
-                txt_passe.set("")
-                txt_cpasse.set("")
+                tsmg.showerror("Error","Both Password Are Different")   
+                uname.set("")
+                pwd.set("")
+                cpwd.set("") 
+
+uname=StringVar()
+pwd=StringVar()
+cpwd=StringVar()
+
+root.geometry("700x500")
+root.title("Welcome To My Page")
+
+f=Frame(root)
+Label(f,text="Login To Continue",font="SegoeUI 18 bold",pady=10).pack()
+f.pack()
+
+f=Frame(root)
+Label(f,text="Username",font="SegoeUI 14 bold",pady=5).pack()
+e1=Entry(f,textvariable=uname,font="SegoeUI 14 bold",borderwidth=5,relief=SUNKEN).pack(padx=5,pady=5)
+Label(f,text="Password",font="SegoeUI 14 bold",pady=5).pack()
+e2=Entry(f,textvariable=pwd,font="SegoeUI 14 bold",borderwidth=5,relief=SUNKEN).pack(padx=5,pady=5)
+l1=Label(f,text="Conform-Password",font="SegoeUI 14 bold",pady=5).pack()
+e3=Entry(f,textvariable=cpwd,font="SegoeUI 14 bold",borderwidth=5,relief=SUNKEN).pack(padx=5,pady=5)
+f.pack()
+
+f=Frame(root)
+b1=Button(f,text="Login",font="SegoeUI 10 bold",command=check).pack(side=LEFT,pady=10,padx=10)
+b2=Button(f,text="Sign-Up",font="SegoeUI 10 bold",command=save).pack(side=LEFT,pady=10,padx=10)
+f.pack()
+
+f=Frame(root)
+Label(f,text="Don't Have An Account Then Sign-Up",font="SegoeUI 14 bold",pady=5).pack()
+f.pack()
+
+root.mainloop()
--- projeto.py
+++ projeto.py
@@ -12,42 +12,104 @@ f.close()
 def Login():
     user = txt_utilizador.get()
     password = txt_passe.get()
-    
+
     guardar = user + ";" + password
     f = open("basedados.txt","r")
     lista = f.readlines()
 
-    if str(guardar) in str(lista):
-        messagebox.showinfo("Bem vindo",f"Olá {user}, o seu login foi efetuado com sucesso")
+    if user == "" or password == "":
+        messagebox.showerror("Erro","Por favor forneça os seus dados de Acesso")
     else:
-        messagebox.showerror("Erro","Utilizador não encontrado. Por favor crie conta.")
+        if str(guardar) in str(lista):
+            messagebox.showinfo("Bem vindo",f"Olá {user}, o seu login foi efetuado com sucesso")
+        else:
+            messagebox.showerror("Tentativa de Login sem sucesso","Utilizador ou palavra-passe incorreta. Por favor tente novamente ou crie conta.")
+            txt_passe.delete(0,"end")
+                
+#FUNÇÃO NOVA JANELA
+def novaJanela():
+    nJanela = Toplevel(window)
+    nJanela.title("Criar Conta")
 
-    txt_utilizador.set("")
-    txt_passe.set("")
+    #CENTRAR JANELA
+    w = 450
+    h = 230
+    ws = nJanela.winfo_screenwidth()
+    hs = nJanela.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    nJanela.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    #LABEL UTILIZADOR
+    lbl_utilizador_criar=Label(nJanela,text="Utilizador:",fg="black",font=("Times New Roman",14))
+    lbl_utilizador_criar.place(x=30,y=20)
+
+    #ENTRY UTILIZADOR
+    txt_utilizador_criar=Entry(nJanela, textvariable = user_criar, width=30)
+    txt_utilizador_criar.place(x=230,y=22)
+
+    #LABEL E-MAIL
+    lbl_email=Label(nJanela,text="Email:",fg="black",font=("Times New Roman",14))
+    lbl_email.place(x=30,y=50)
+
+    #ENTRY E-MAIL
+    txt_email=Entry(nJanela,textvariable = email, width=30)
+    txt_email.place(x=230,y=53)
+
+    #LABEL PALAVRA-PASSE
+    lbl_passe_criar=Label(nJanela,text="Palavra-Passe:",fg="black",font=("Times New Roman",14))
+    lbl_passe_criar.place(x=30,y=80)
+
+    #ENTRY PALAVRA-PASSE
+    txt_passe_criar=Entry(nJanela,textvariable = passe_criar, width=30,show="*")
+    txt_passe_criar.place(x=230,y=83)
+
+    #LABEL CONFIRMAR PALAVRA-PASSE
+    lbl_cpasse=Label(nJanela,text="Confirmar Palavra-Passe:",fg="black",font=("Times New Roman",14))
+    lbl_cpasse.place(x=30,y=110)
+
+    #ENTRY PALAVRA-PASSE
+    txt_cpasse=Entry(nJanela,textvariable = cpasse, width=30,show="*")
+    txt_cpasse.place(x=230,y=113)
+
+    #BOTÃO CRIAR CONTA
+    btn_criarconta = Button(nJanela, text = "Criar Conta", fg = "blue", font = ("Calibri", 12),width=15,height=1, command = CriarConta)
+    btn_criarconta.place (x = 160, y = 160)
+
+user = StringVar()
+user_criar = StringVar()
+email = StringVar()
+passe = StringVar()
+passe_criar = StringVar()
+cpasse = StringVar()
 
 #FUNÇÃO CRIAR CONTA
 def CriarConta():
-    user = txt_utilizador.get()
-    password = txt_passe.get()
+    utilizador = user_criar.get()
+    password = passe.get()
+    email1 = email.get()
+    cpassword = cpasse.get()
 
-    guardar = user + ";" + password
+    guardar = utilizador + ";" + email1 + ";" + password 
     f = open("basedados.txt","r")
-    if user != "" and password != "":
+
+    if utilizador != "" and password != "":
         lista = f.readlines()
+
         if str(guardar) in str(lista):
             messagebox.showerror("Erro","Já existe uma conta com esses dados, por favor efetue login")
-            txt_utilizador.set("")
-            txt_passe.set("")
-        elif str(user) in str(lista):
+            
+        elif str(utilizador) in str(lista):
             messagebox.showerror("Erro","Esse utilizador já existe")
-            txt_utilizador.set("")
-            txt_passe.set("")
+            
         else:
+            if password == cpassword:
                 f = open("basedados.txt","a")
-                f.write(f"{user};{password}\n")
+                f.write(f"{user};{email};{password}\n")
                 messagebox.showinfo("Sucesso","A sua conta foi criada com sucesso!")
-                txt_utilizador.set("")
-                txt_passe.set("")
+                
+            else:
+                messagebox.showerror("Erro","As duas passwords não coincidem")
 
 #CENTRAR JANELA PRINCIPAL
 w = 500
@@ -68,19 +130,18 @@ txt_utilizador.place(x=240,y=22)
 
 #LABEL PALAVRA-PASSE
 lbl_passe=Label(window,text="Palavra-Passe:",fg="black",font=("Times New Roman",14))
-lbl_passe.place(x=150,y=50)
+lbl_passe.place(x=120,y=50)
 
 #ENTRY PALAVRA-PASSE
 txt_passe=Entry(window,width=20,show="*")
-txt_passe.place(x=265,y=53)
+txt_passe.place(x=240,y=53)
 
 #BOTÃO PARA LOGIN
 btn_login = Button(window, text = "Login", fg = "green", font = ("Calibri", 12),width=15,height=1, command = Login)
 btn_login.place (x = 190, y = 100)
 
 #BOTÃO PARA CRIAR CONTA
-btn_criar = Button(window, text = "Criar Conta", fg = "blue", font = ("Calibri", 12),width=15,height=1, command = CriarConta)
+btn_criar = Button(window, text = "Criar Conta", fg = "blue", font = ("Calibri", 12),width=15,height=1, command = novaJanela)
 btn_criar.place (x = 190, y = 140)
-
 
 window.mainloop()
