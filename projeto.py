d0af993182f614e24b02c1e08af54dc243b912c2
Lucas <lucasiilva03@gmail.com>
2021-12-29 15:36:04 +0000

    PROJETO CORRETO

--- projeto.py
+++ projeto.py
@@ -8,6 +8,71 @@ window.title("Gestor de Roteiro de Viagens")
 f = open("basedados.txt","a")
 f.close()
 
+#FUNÇÃO DA JANELA PRINCIPAL 
+def mainWindow():
+
+    #CENTRAR JANELA PRINCIPAL
+    w = 500
+    h = 200
+    ws = window.winfo_screenwidth()
+    hs = window.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    #INSERE A LABEL E ENTRY DO UTILIZADOR
+    lbl_utilizador.place(x=120,y=20)
+    txt_utilizador.place(x=230,y=22)
+
+    #INSERE A LABEL E ENTRY DA PALAVRA PASSE
+    lbl_passe.place(x=90,y=50)
+    txt_passe.place(x=230,y=53)
+
+    #INSERE OS BOTÕES DE LOGIN E DE CRIAR CONTA
+    btn_login.place (x=190,y=100)
+    btn_criarconta.place (x = 190, y = 140)
+
+    #ESQUECE O POSICIONAMENTO DAS LABELS EMAIL, CPASSE E DO BOTÃO CRIAR CONTA (SEM A FUNÇÃO)
+    lbl_email.place_forget()
+    txt_email.place_forget()
+    lbl_cpasse.place_forget() 
+    txt_cpasse.place_forget()
+    btn_criar.place_forget()
+
+#FUNÇÃO PARA JANELA DE CRIAR CONTA
+def nJanela():
+
+    #ALTERAR AS DIMENSÕES DA JANELA PARA INSERIR NOVOS CAMPOS
+    w = 450
+    h = 230
+    ws = window.winfo_screenwidth()
+    hs = window.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    #PLACE DA ENTRY UTILIZADOR
+    lbl_utilizador.place(x=30,y=20)
+    txt_utilizador.place(x=230,y=22)
+
+    #PLACE DA ENTRY EMAIL
+    lbl_email.place(x=30,y=50)
+    txt_email.place(x=230,y=53)
+
+    #PLACE DA ENTRE PASSWORD
+    lbl_passe.place(x=30,y=80)
+    txt_passe.place(x=230,y=83)
+
+    #PLACE DA ENTRY PARA CONFIRMAR PASSOWRD
+    lbl_cpasse.place(x=30,y=110)
+    txt_cpasse.place(x=230,y=113)
+
+    #APAGAR OS BOTÕES DE LOGIN E DE CRIAR CONTA
+    btn_login.place_forget()
+    btn_criarconta.place_forget()
+
+    btn_criar.place(x=160,y=160)
+
 #FUNÇÃO LOGIN
 def Login():
     user = txt_utilizador.get()
@@ -52,40 +117,28 @@ def CriarConta():
                 f = open("basedados.txt","a")
                 f.write(utilizador + ";" + email + ";" + password + ";" + "user""\n")
                 messagebox.showinfo("Sucesso","A sua conta foi criada com sucesso!")
-                
+
+                mainWindow()
             else:
                 messagebox.showerror("Erro","As duas passwords não coincidem")
 
-#CENTRAR JANELA PRINCIPAL
-w = 500
-h = 200
-ws = window.winfo_screenwidth()
-hs = window.winfo_screenheight()
-x = (ws/2) - (w/2)
-y = (hs/2) - (h/2)
-window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 #------------------EFETUAR LOGIN------------------#
 
 #LABEL UTILIZADOR
 lbl_utilizador=Label(window,text="Utilizador:",fg="black",font=("Times New Roman",14))
-lbl_utilizador.place(x=120,y=20)
+
 
 #ENTRY UTILIZADOR
 txt_utilizador=Entry(window,width=30)
-txt_utilizador.place(x=230,y=22)
+
 
 #LABEL PALAVRA-PASSE
 lbl_passe=Label(window,text="Palavra-Passe:",fg="black",font=("Times New Roman",14))
-lbl_passe.place(x=90,y=50)
+
 
 #ENTRY PALAVRA-PASSE
 txt_passe=Entry(window,width=30,show="*")
-txt_passe.place(x=230,y=53)
-
-#BOTÃO PARA LOGIN
-btn_login = Button(window, text = "Login", fg = "green", font = ("Calibri", 12),width=15,height=1, command = Login)
-btn_login.place (x=190,y=100)
 
 #------------------CRIAR UTILIZADOR------------------#
 
@@ -101,36 +154,14 @@ lbl_cpasse=Label(window,text="Confirmar Palavra-Passe:",fg="black",font=("Times 
 #ENTRY CONFIRMAR PALAVRA-PASSE
 txt_cpasse=Entry(window, width=30,show="*")
 
-def nJanela():
-    w = 450
-    h = 230
-    ws = window.winfo_screenwidth()
-    hs = window.winfo_screenheight()
-    x = (ws/2) - (w/2)
-    y = (hs/2) - (h/2)
-    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-    lbl_utilizador.place(x=30,y=20)
-    txt_utilizador.place(x=230,y=22)
-
-    lbl_email.place(x=30,y=50)
-    txt_email.place(x=230,y=53)
-
-    lbl_passe.place(x=30,y=80)
-    txt_passe.place(x=230,y=83)
-
-    lbl_cpasse.place(x=30,y=110)
-    txt_cpasse.place(x=230,y=113)
-
-    btn_login.destroy()
-    btn_criarconta.destroy()
-
-    btn_criar = Button(window,text="Criar Conta", fg="blue", font = ("Calibri",12), width=15,height=1, command=CriarConta)
-    btn_criar.place(x=160,y=160)
+#BOTÃO PARA LOGIN
+btn_login = Button(window, text = "Login", fg = "green", font = ("Calibri", 12),width=15,height=1, command = Login)
 
 #BOTÃO CRIAR CONTA
 btn_criarconta = Button(window, text = "Criar Conta", fg = "blue", font = ("Calibri", 12),width=15,height=1, command = nJanela)
-btn_criarconta.place (x = 190, y = 140)
 
+#BOTÃO DE CRIAR CONTA QUE EXECUTA A FUNÇÃO
+btn_criar = Button(window,text="Criar Conta", fg="blue", font = ("Calibri",12), width=15,height=1, command=CriarConta)
 
+mainWindow()
 window.mainloop()
